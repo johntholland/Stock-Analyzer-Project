a2deb8419c1b3638fc5d955a57eb0e96d56b55c8
John Holland <jthfortyone@gmail.com>
2013-08-11 22:44:36 -0500

    Added a text file to work from
    The text file is incomplete but it is a start

--- StockAnalyzer/Form1.cs
+++ StockAnalyzer/Form1.cs
@@ -59,7 +59,7 @@ namespace StockAnalyzer
                 Console.WriteLine("Worksheet could not be created. Check that your office installation and project references are correct.");
                 return;
             }
-            
+
             String urlbase = "https://www.google.com/finance?q=";
 
 
@@ -79,7 +79,7 @@ namespace StockAnalyzer
 
             //paste desired return %
             this.excel.ActiveSheet.Cells[31, 5] = 10;
-            
+
             var bankpage = webGet.Load(urlbase);
             var banktest = bankpage.DocumentNode.SelectSingleNode("//*[@id='appbar']/div/div[2]/div[1]/span");
 
@@ -94,121 +94,121 @@ namespace StockAnalyzer
 
             else
             {
-            document = webGet.Load(url);
+                document = webGet.Load(url);
 
-            //get Company's full name
-            var htest = document.DocumentNode.SelectSingleNode("//*[@id='appbar']/div/div[2]/div[1]/span");
-            if (htest != null)
-                this.excel.ActiveSheet.Cells[3, 3] = htest.InnerText.Trim();
-            else
-            {
-                errorOccured();
-            }
+                //get Company's full name
+                var htest = document.DocumentNode.SelectSingleNode("//*[@id='appbar']/div/div[2]/div[1]/span");
+                if (htest != null)
+                    this.excel.ActiveSheet.Cells[3, 3] = htest.InnerText.Trim();
+                else
+                {
+                    errorOccured();
+                }
 
-            //paste provided stock symbol 
-            this.excel.ActiveSheet.Cells[4, 3] = textBox1.Text.ToUpper();
+                //paste provided stock symbol 
+                this.excel.ActiveSheet.Cells[4, 3] = textBox1.Text.ToUpper();
 
-            //get multiplier
-            var multiplier = document.DocumentNode.SelectSingleNode("//*[@id='fs-table']/thead/tr/th[1]");
-            if (multiplier != null)
-                this.excel.ActiveSheet.Cells[6, 7] = multiplier.InnerText.Trim();
-            else
-            {
-                errorOccured();
-            }
+                //get multiplier
+                var multiplier = document.DocumentNode.SelectSingleNode("//*[@id='fs-table']/thead/tr/th[1]");
+                if (multiplier != null)
+                    this.excel.ActiveSheet.Cells[6, 7] = multiplier.InnerText.Trim();
+                else
+                {
+                    errorOccured();
+                }
 
-            //get dates Total Revenue & Net Income (annual)
-            var datesAnnual  = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/thead/tr/th[2]");
-            this.excel.ActiveSheet.Cells[8, 4] = datesAnnual.InnerText.Trim();
-            var datesAnnual1 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/thead/tr/th[3]");
-            this.excel.ActiveSheet.Cells[8, 5] = datesAnnual1.InnerText.Trim();
-            var datesAnnual2 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/thead/tr/th[4]");
-            this.excel.ActiveSheet.Cells[8, 6] = datesAnnual2.InnerText.Trim();
-            var datesAnnual3 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/thead/tr/th[5]");
-            this.excel.ActiveSheet.Cells[8, 7] = datesAnnual3.InnerText.Trim();
+                //get dates Total Revenue & Net Income (annual)
+                var datesAnnual = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/thead/tr/th[2]");
+                this.excel.ActiveSheet.Cells[8, 4] = datesAnnual.InnerText.Trim();
+                var datesAnnual1 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/thead/tr/th[3]");
+                this.excel.ActiveSheet.Cells[8, 5] = datesAnnual1.InnerText.Trim();
+                var datesAnnual2 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/thead/tr/th[4]");
+                this.excel.ActiveSheet.Cells[8, 6] = datesAnnual2.InnerText.Trim();
+                var datesAnnual3 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/thead/tr/th[5]");
+                this.excel.ActiveSheet.Cells[8, 7] = datesAnnual3.InnerText.Trim();
 
-            //get Total Revenue (annual)
-            var totalRevenueAnnual  = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[3]/td[2]");
-            this.excel.ActiveSheet.Cells[9, 4] = totalRevenueAnnual.InnerText.Trim();
-            var totalRevenueAnnual1 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[3]/td[3]");
-            this.excel.ActiveSheet.Cells[9, 5] = totalRevenueAnnual1.InnerText.Trim();
-            var totalRevenueAnnual2 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[3]/td[4]");
-            this.excel.ActiveSheet.Cells[9, 6] = totalRevenueAnnual2.InnerText.Trim();
-            var totalRevenueAnnual3 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[3]/td[5]");
-            this.excel.ActiveSheet.Cells[9, 7] = totalRevenueAnnual3.InnerText.Trim();
+                //get Total Revenue (annual)
+                var totalRevenueAnnual = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[3]/td[2]");
+                this.excel.ActiveSheet.Cells[9, 4] = totalRevenueAnnual.InnerText.Trim();
+                var totalRevenueAnnual1 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[3]/td[3]");
+                this.excel.ActiveSheet.Cells[9, 5] = totalRevenueAnnual1.InnerText.Trim();
+                var totalRevenueAnnual2 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[3]/td[4]");
+                this.excel.ActiveSheet.Cells[9, 6] = totalRevenueAnnual2.InnerText.Trim();
+                var totalRevenueAnnual3 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[3]/td[5]");
+                this.excel.ActiveSheet.Cells[9, 7] = totalRevenueAnnual3.InnerText.Trim();
 
-            //*[@id="fs-table"]/tbody/tr[28]/td[2]
+                //*[@id="fs-table"]/tbody/tr[28]/td[2]
 
-            //get Net Income 
-            var netIncomeAnnual  = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[28]/td[2]");
-            this.excel.ActiveSheet.Cells[10, 4] = netIncomeAnnual.InnerText.Trim();
-            var netIncomeAnnual1 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[28]/td[3]");
-            this.excel.ActiveSheet.Cells[10, 5] = netIncomeAnnual1.InnerText.Trim();
-            var netIncomeAnnual2 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[28]/td[4]");
-            this.excel.ActiveSheet.Cells[10, 6] = netIncomeAnnual2.InnerText.Trim();
-            var netIncomeAnnual3 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[28]/td[5]");
-            this.excel.ActiveSheet.Cells[10, 7] = netIncomeAnnual3.InnerText.Trim();
+                //get Net Income 
+                var netIncomeAnnual = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[28]/td[2]");
+                this.excel.ActiveSheet.Cells[10, 4] = netIncomeAnnual.InnerText.Trim();
+                var netIncomeAnnual1 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[28]/td[3]");
+                this.excel.ActiveSheet.Cells[10, 5] = netIncomeAnnual1.InnerText.Trim();
+                var netIncomeAnnual2 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[28]/td[4]");
+                this.excel.ActiveSheet.Cells[10, 6] = netIncomeAnnual2.InnerText.Trim();
+                var netIncomeAnnual3 = document.DocumentNode.SelectSingleNode("//div[@id='incannualdiv']/table[@id='fs-table']/tbody/tr[28]/td[5]");
+                this.excel.ActiveSheet.Cells[10, 7] = netIncomeAnnual3.InnerText.Trim();
 
-            //get dates Total Revenue & Net Income (Quarterly)
-            var datesQuarterly  = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/thead/tr/th[2]");
-            this.excel.ActiveSheet.Cells[20, 6] = datesQuarterly.InnerText.Trim();
-            var datesQuarterly1 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/thead/tr/th[3]");
-            this.excel.ActiveSheet.Cells[20, 7] = datesQuarterly1.InnerText.Trim();
-            var datesQuarterly2 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/thead/tr/th[4]");
-            this.excel.ActiveSheet.Cells[20, 8] = datesQuarterly2.InnerText.Trim();
-            var datesQuarterly3 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/thead/tr/th[5]");
-            this.excel.ActiveSheet.Cells[20, 9] = datesQuarterly3.InnerText.Trim();
+                //get dates Total Revenue & Net Income (Quarterly)
+                var datesQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/thead/tr/th[2]");
+                this.excel.ActiveSheet.Cells[20, 6] = datesQuarterly.InnerText.Trim();
+                var datesQuarterly1 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/thead/tr/th[3]");
+                this.excel.ActiveSheet.Cells[20, 7] = datesQuarterly1.InnerText.Trim();
+                var datesQuarterly2 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/thead/tr/th[4]");
+                this.excel.ActiveSheet.Cells[20, 8] = datesQuarterly2.InnerText.Trim();
+                var datesQuarterly3 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/thead/tr/th[5]");
+                this.excel.ActiveSheet.Cells[20, 9] = datesQuarterly3.InnerText.Trim();
 
-            //get Total Revenue (Quarterly)
-            var totalRevenueQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[3]/td[2]");
-            this.excel.ActiveSheet.Cells[21, 6] = totalRevenueQuarterly.InnerText.Trim();
-            var totalRevenueQuarterly1 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[3]/td[3]");
-            this.excel.ActiveSheet.Cells[21, 7] = totalRevenueQuarterly1.InnerText.Trim();
-            var totalRevenueQuarterly2 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[3]/td[4]");
-            this.excel.ActiveSheet.Cells[21, 8] = totalRevenueQuarterly2.InnerText.Trim();
-            var totalRevenueQuarterly3 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[3]/td[5]");
-            this.excel.ActiveSheet.Cells[21, 9] = totalRevenueQuarterly3.InnerText.Trim();
+                //get Total Revenue (Quarterly)
+                var totalRevenueQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[3]/td[2]");
+                this.excel.ActiveSheet.Cells[21, 6] = totalRevenueQuarterly.InnerText.Trim();
+                var totalRevenueQuarterly1 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[3]/td[3]");
+                this.excel.ActiveSheet.Cells[21, 7] = totalRevenueQuarterly1.InnerText.Trim();
+                var totalRevenueQuarterly2 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[3]/td[4]");
+                this.excel.ActiveSheet.Cells[21, 8] = totalRevenueQuarterly2.InnerText.Trim();
+                var totalRevenueQuarterly3 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[3]/td[5]");
+                this.excel.ActiveSheet.Cells[21, 9] = totalRevenueQuarterly3.InnerText.Trim();
 
-            //get Net Income (Quarterly)
+                //get Net Income (Quarterly)
 
-            var netIncomeQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[28]/td[2]");
-            this.excel.ActiveSheet.Cells[22, 6] = netIncomeQuarterly.InnerText.Trim();
-            var netIncomeQuarterly1 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[28]/td[3]");
-            this.excel.ActiveSheet.Cells[22, 7] = netIncomeQuarterly1.InnerText.Trim();
-            var netIncomeQuarterly2 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[28]/td[4]");
-            this.excel.ActiveSheet.Cells[22, 8] = netIncomeQuarterly2.InnerText.Trim();
-            var netIncomeQuarterly3 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[28]/td[5]");
-            this.excel.ActiveSheet.Cells[22, 9] = netIncomeQuarterly3.InnerText.Trim();
+                var netIncomeQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[28]/td[2]");
+                this.excel.ActiveSheet.Cells[22, 6] = netIncomeQuarterly.InnerText.Trim();
+                var netIncomeQuarterly1 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[28]/td[3]");
+                this.excel.ActiveSheet.Cells[22, 7] = netIncomeQuarterly1.InnerText.Trim();
+                var netIncomeQuarterly2 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[28]/td[4]");
+                this.excel.ActiveSheet.Cells[22, 8] = netIncomeQuarterly2.InnerText.Trim();
+                var netIncomeQuarterly3 = document.DocumentNode.SelectSingleNode("//div[@id='incinterimdiv']/table[@id='fs-table']/tbody/tr[28]/td[5]");
+                this.excel.ActiveSheet.Cells[22, 9] = netIncomeQuarterly3.InnerText.Trim();
 
-            //get debts Date current as of
-            var currentLiabilitiesDateQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='balinterimdiv']/table[@id='fs-table']/thead/tr/th[2]");
-            this.excel.ActiveSheet.Cells[37, 7] = currentLiabilitiesDateQuarterly.InnerText.Trim();
-            this.excel.ActiveSheet.Cells[37, 8] = currentLiabilitiesDateQuarterly.InnerText.Trim();
-           
-            //get current Debts
-            var currentLiabilitiesQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='balinterimdiv']/table[@id='fs-table']/tbody/tr[26]/td[2]");
-            this.excel.ActiveSheet.Cells[39, 7] = currentLiabilitiesQuarterly.InnerText.Trim();
-            var currentOtherLiabilitiesQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='balinterimdiv']/table[@id='fs-table']/tbody/tr[30]/td[2]");
-            this.excel.ActiveSheet.Cells[39, 8] = currentOtherLiabilitiesQuarterly.InnerText.Trim();
+                //get debts Date current as of
+                var currentLiabilitiesDateQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='balinterimdiv']/table[@id='fs-table']/thead/tr/th[2]");
+                this.excel.ActiveSheet.Cells[37, 7] = currentLiabilitiesDateQuarterly.InnerText.Trim();
+                this.excel.ActiveSheet.Cells[37, 8] = currentLiabilitiesDateQuarterly.InnerText.Trim();
 
-            //get current outstanding shares
-            //*[@id="fs-table"]/tbody/tr[42]/td[2]
-            var currentOutstandingShares = document.DocumentNode.SelectSingleNode("//div[@id='balinterimdiv']/table[@id='fs-table']/tbody/tr[42]/td[2]");
-            this.excel.ActiveSheet.Cells[47, 5] = currentOutstandingShares.InnerText.Trim();
-            
+                //get current Debts
+                var currentLiabilitiesQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='balinterimdiv']/table[@id='fs-table']/tbody/tr[26]/td[2]");
+                this.excel.ActiveSheet.Cells[39, 7] = currentLiabilitiesQuarterly.InnerText.Trim();
+                var currentOtherLiabilitiesQuarterly = document.DocumentNode.SelectSingleNode("//div[@id='balinterimdiv']/table[@id='fs-table']/tbody/tr[30]/td[2]");
+                this.excel.ActiveSheet.Cells[39, 8] = currentOtherLiabilitiesQuarterly.InnerText.Trim();
 
-            excel.Visible = true;
-            try
-            {
-                wb.SaveAs(textBox1.Text, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-            }
-            catch
-            {
-                toolStripStatusLabel1.Text = "The workbook was unable to be saved. do you have a workbook by the same name open already?";
-                toolStripStatusLabel1.Visible = true;
+                //get current outstanding shares
+                //*[@id="fs-table"]/tbody/tr[42]/td[2]
+                var currentOutstandingShares = document.DocumentNode.SelectSingleNode("//div[@id='balinterimdiv']/table[@id='fs-table']/tbody/tr[42]/td[2]");
+                this.excel.ActiveSheet.Cells[47, 5] = currentOutstandingShares.InnerText.Trim();
+
+
+                excel.Visible = true;
+                try
+                {
+                    wb.SaveAs(textBox1.Text, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                }
+                catch
+                {
+                    toolStripStatusLabel1.Text = "The workbook was unable to be saved. do you have a workbook by the same name open already?";
+                    toolStripStatusLabel1.Visible = true;
+                }
             }
         }
-
         //private void createTextListing()
         //{
         //    //*[@id="ctl00_cph1_divSymbols"]/table/tbody/tr[2]/td[1]/a
